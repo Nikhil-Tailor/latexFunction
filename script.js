c53dbb5923c8d04c2c0efe0f23935de63db00b06
Nikhil Tailor <76070429+Nikhil-Tailor@users.noreply.github.com>
2024-02-22 12:19:59 +0000

    Updated itemize function to make it clearer and more concise

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 function convertText() {
     let inputText = document.querySelector('#input').value;
-    // Your conversion function goes here
     let outputText = itemize(inputText);
     document.querySelector('#output').value = outputText;
 }
 
 function itemize(text) {
-    returnText = '\\begin{itemize}';
-    text.split('\n').forEach(function (line) {
-            returnText += '\n\\item ' + line;
-        }
-    );
-    returnText += '\n\\end{itemize}';
-
-    return returnText;
+    let items = text.split('\n').map(line => `\\item ${line}`).join('\n');
+    return `\\begin{itemize}\n${items}\n\\end{itemize}`;
 }
